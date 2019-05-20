9c169221d2b84b5a075f3b294d71f375ce6639d4
xief <a64180120@163.com>
2019-05-20 10:17:27 +0800

    忽略config

--- src/utils/config.js
+++
@@ -1,6 +0,0 @@
-// axios中请求配置有baseURL选项，表示请求URL公共部分。
-// `baseURL` 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL
-
-export const baseURL = 'http://10.0.13.52:8030/api/DMP'
-//export const baseURL = 'http://218.108.53.106:8030/api/DMP'
-//export const baseURL =window.global.baseUrl
