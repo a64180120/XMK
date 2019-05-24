01934b4fcfd6b82a1a39b7bf6b6e7624a6c61c68
xief <a64180120@163.com>
2019-05-24 09:29:41 +0800

    支付口令

--- src/pages/systemsetting/dataSafe/dictionary.vue
+++ src/pages/systemsetting/dataSafe/dictionary.vue
@@ -8,17 +8,60 @@
                 </div>
             </div>
         </topHandle>
+        <div class="container">
+            <div class="type">
+                <p>字典类型</p>
+                <ul>
+                    <li>支付方式</li>
+                </ul>
+            </div>
+            <div class="content">
+                <ul>
+                    <li>序号</li>
+                    <li>类型编码</li>
+                    <li>类型名称</li>
+                    <li>备注</li>
+                    <li>启用/停用</li>
+                </ul>   
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import topHandle from '@/components/topNav/topHandle'
 export default {
-    name:'dictionary'
+    name:'dictionary',
+    components:{
+        topHandle
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .dictionary{
+    .btnCon{
 
+        .handle{
+            display: inline-block;
+            width: 60px;
+            height: 50px;
+            padding: 0px 0;
+            cursor: pointer;
+            &:active{
+                background: #ccc;
+            }
+        }
+    }
+    .topIcon{
+        height:60%;
+        >img{
+            height:100%;
+        }
+    }
+    .container{
+        left:200px;
+        top:150px;    
+    }
 }
 </style>
