8dd8f29c8293a4e59289598b9a2cdb4fce116e69
xief <a64180120@163.com>
2019-06-05 09:23:15 +0800

    1

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -39,7 +39,7 @@ function apiAxios(method, url, params, hastimeout) {
     // oracle 数据库
     var appKey = 'D31B7F91-3068-4A49-91EE-F3E13AE5C48C',
         appSecret = '103CB639-840C-4E4F-8812-220ECE3C4E4D',
-        url1 = "http://127.0.0.1:8099",
+        url1 = "http://127.0.0.1:8031",
         reqTimeStamp = Date.parse(new Date());
 
     var appinfo = {
