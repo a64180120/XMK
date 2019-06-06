c0f55b12df06407e9f53978d6ab0ea112439a7cb
hongyz <123@123>
2019-06-06 11:56:35 +0800

    项目新增修改

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -20,7 +20,7 @@ function apiAxios(method, url, params, hastimeout) {
 
   var appinfo = {
     //sql 数据库
-   /* DbName: 'NG0001',
+/*    DbName: 'NG0001',
     OrgId: '547181121000001',
     UserId: '521180820000001',*/
     DbName: 'NG0006',
