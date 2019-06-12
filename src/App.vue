7402f87ee2fa63d525343c8692c7b680e66108ba
xief <a64180120@163.com>
2019-06-12 11:55:25 +0800

    beforecreate 没用...!!!

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,9 @@ export default {
       isLogin: state => state.isLogin
     })
   },
-  beforeCreate(){
-    this.getUserInfo();
-  },
+ 
   created() {
-    
+     this.getUserInfo();
   },
   // TODO: 全局状态加载及变更。请根据实际情况改写
   beforeMount() {
@@ -66,6 +64,7 @@ export default {
           })
       },
     getUserInfo(){ //获取url传参
+      console.log(32313123000)
       let index = window.location.href.indexOf('?');
       let ind= window.location.href.indexOf('#');
       let info=window.location.href.slice(index+1).slice(0,ind-index-1).split('&');
