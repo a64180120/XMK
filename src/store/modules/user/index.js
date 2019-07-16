aec3f8ca03730fb524ee900667d83be2a8c98feb
xief <a64180120@163.com>
2019-07-16 17:06:42 +0800

    1

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -3,138 +3,138 @@ import Vue from 'vue'
 
 //状态
 const state = {
-    //签名
-    token: "",
-    //用户id
-    userid: "",
-    //用户code
-    usercode:'',
-    //用户名称
-    username: "",
-    //组织列表
-    orglist:[],
-    //当前选中组织
-    orgid:'', //id
-    orgcode:'', //编码
-    orgname:'',//名称
-    /*当前选中部门*/
-    bmid:'',
-    bmcode:'',
-    bmname:'',
-    year:'',//年份
-    appinfo:{},//ajax登录信息
-    menubutton:{},//菜单按钮权限
+  //签名
+  token: "",
+  //用户id
+  userid: "",
+  //用户code
+  usercode: '',
+  //用户名称
+  username: "",
+  //组织列表
+  orglist: [],
+  //当前选中组织
+  orgid: '', //id
+  orgcode: '', //编码
+  orgname: '',//名称
+  /*当前选中部门*/
+  bmid: '',
+  bmcode: '',
+  bmname: '',
+  year: '',//年份
+  appinfo: {},//ajax登录信息
+  menubutton: {},//菜单按钮权限
 };
 
 //计算获取取新数据
 const getters = {
-    getToken: function (state) {
-        return state.token
-    }
+  getToken: function (state) {
+    return state.token
+  }
 };
 
 //操作state(同步)
 const mutations = {
-    //token
-    setToken: (state, data) => {
-        if (data) {
-            state.token = data.token;
-            //缓存token信息
-            Session.setToken(data);
-        } else {
-            //删除缓存信息
-            state.token='';
-            Session.removeToken();
-        }
-    },
-    //ajax的appinfo信息
-    setAppinfo(state,data){
-        state.appinfo=data;
-        state.userid=data.UserId;
-        state.orgid=data.OrgId;
-        state.year=data.Year
-    },
-    //组织用户的缓存信息
-    setLoginInfo(state,data){
-        state.userid=data.User.PhId,
-        state.usercode=data.User.UserNo,
-        state.username=data.User.UserName,
-        state.orgid=data.Org.PhId,
-        state.orgcode=data.Org.OCode,
-        state.orgname=data.Org.OName,
-        state.menubutton=data.MenuButton
-    },
-    //组织列表
-    setOrglist(state,data){
-        state.orglist=data;
-    },
-    //修改当前组织
-    setOrganize(state,data){
-        state.orgid=data.PhId;
-        state.orgcode=data.OCode;
-        state.orgname=data.OName;
-
-    },
-  //修改当前部门
-  setBm(state,data){
-    state.bmid=data.PhId;
-    state.bmcode=data.OCode;
-    state.bmname=data.OName;
+  //token
+  setToken: (state, data) => {
+    if (data) {
+      state.token = data.token;
+      //缓存token信息
+      Session.setToken(data);
+    } else {
+      //删除缓存信息
+      state.token = '';
+      Session.removeToken();
+    }
+  },
+  //ajax的appinfo信息
+  setAppinfo (state, data) {
+    state.appinfo = data;
+    state.userid = data.UserId;
+    state.orgid = data.OrgId;
+    state.year = data.Year
+  },
+  //组织用户的缓存信息
+  setLoginInfo (state, data) {
+    state.userid = data.User.PhId,
+      state.usercode = data.User.UserNo,
+      state.username = data.User.UserName,
+      state.orgid = data.Org.PhId,
+      state.orgcode = data.Org.OCode,
+      state.orgname = data.Org.OName,
+      state.menubutton = data.MenuButton
+  },
+  //组织列表
+  setOrglist (state, data) {
+    state.orglist = data;
+  },
+  //修改当前组织
+  setOrganize (state, data) {
+    state.orgid = data.PhId;
+    state.orgcode = data.OCode;
+    state.orgname = data.OName;
 
   },
-    //修改年度
-    setYear(state,data){
-        state.year=data
-    }
+  //修改当前部门
+  setBm (state, data) {
+    state.bmid = data.PhId;
+    state.bmcode = data.OCode;
+    state.bmname = data.OName;
+
+  },
+  //修改年度
+  setYear (state, data) {
+    state.year = data
+  }
 };
 
 //执行一些方法(异步)
 const actions = {
-    // 获取Token
-    getToken({ commit, state }) {
-        return new Promise((resolve, reject) => {
+  // 获取Token
+  getToken ({ commit, state }) {
+    return new Promise((resolve, reject) => {
 
-            // Vue.prototype.getAxios('/Token/GetToken',{ sysno:'N3App'},true)
-            // .then(res => {
-            //     if (res.Status === "success") {
-            //         //缓存token信息
-            //         var response=res.Data;
-            //         var object = {
-            //             token: response.SignToken,
-            //             year:response.Year,
-            //             account:response.Account,
-            //         };
-            //         //用户信息缓存
-            //         commit("setToken", object);
-            //     }
-            //     resolve(res);
-            // }).catch(error =>{
-            //     console.log(error)
-            //     reject(error)
-            // });
-        });
-    },
-    // 退出
-    logout({state, commit,dispatch }) {
+      // Vue.prototype.getAxios('/Token/GetToken',{ sysno:'N3App'},true)
+      // .then(res => {
+      //     if (res.Status === "success") {
+      //         //缓存token信息
+      //         var response=res.Data;
+      //         var object = {
+      //             token: response.SignToken,
+      //             year:response.Year,
+      //             account:response.Account,
+      //         };
+      //         //用户信息缓存
+      //         commit("setToken", object);
+      //     }
+      //     resolve(res);
+      // }).catch(error =>{
+      //     console.log(error)
+      //     reject(error)
+      // });
+    });
+  },
+  // 退出
+  logout ({ state, commit, dispatch }) {
 
-        return new Promise(resolve => {
-            //清除缓存
-            commit("setToken", "");
+    return new Promise(resolve => {
+      //清除缓存
+      commit("setToken", "");
 
-            //修改登录状态
-            //若需要在全局命名空间内分发 action 或提交 mutation，将 { root: true } 作为第三参数传给 dispatch 或 commit 即可
-            dispatch('actionIsLogin',false,{root: true})
+      //修改登录状态
+      //若需要在全局命名空间内分发 action 或提交 mutation，将 { root: true } 作为第三参数传给 dispatch 或 commit 即可
+      dispatch('actionIsLogin', false, { root: true })
 
-            resolve();
-        });
-    },
+      resolve();
+    });
+  },
 
 };
 
 export default {
-    namespaced: true,
-    state,
-    getters,
-    mutations,
-    actions
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
 };
