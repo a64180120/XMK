edb428091475d58986349f9266d1ec2f418aeb82
keyao <1044584337@qq.com>
2019-07-18 20:06:25 +0800

    资金拨付新页面

--- src/pages/preproject/preBuildProject.vue
+++ src/pages/preproject/preBuildProject.vue
@@ -6,34 +6,30 @@
         <div class="container content-body">
           <div class="formArea">
             <!--搜索栏-->
-            <div class="btnArea">
-              <el-form>
-                <el-form-item></el-form-item>
+            <div class="btnArea" style="margin-bottom: 15px">
+              <el-form :inline="true">
+                <el-form-item label="年度:">
+                  <el-input size="mini"></el-input>
+                </el-form-item>
+                <el-form-item label="an钮">
+                  <el-button size="mini" @click="fn()">按钮</el-button>
+                </el-form-item>
+                <el-form-item>
+                  <el-button size="mini" @click="fn1()">按钮</el-button>
+                </el-form-item>
               </el-form>
             </div>
             <!--表格区域-->
-            <el-table
-              :data="tableData"
-              :row-class-name="rowClassName"
-              :cell-class-name="cellClassName"
-              :header-cell-class-name="handerCellClassName"
-              @current-change=""
-              :highlight-current-row="highlightCurrentRow"
-              >
-              <el-table-column
-                prop="id"
-                label="编号"
-                align="center">
-
-              </el-table-column>
-              <el-table-column
-                prop="name"
-                label="姓名"></el-table-column>
-              <el-table-column
-                prop="age"  label="年龄"></el-table-column>
-              <el-table-column
-                prop="job" label="工作"></el-table-column>
-            </el-table>
+<!--            <div>-->
+<!--              <data-table-->
+<!--                :data="table.tableData"-->
+<!--                :column="table.colum"-->
+<!--                :selection="table.selection"></data-table>-->
+<!--            </div>-->
+            <div>
+              <item-table
+                :data="table1.tableData"></item-table>
+            </div>
           </div>
         </div>
       </div>
@@ -42,44 +38,120 @@
 
 <script>
     import TopHandle from "../../components/topNav/topHandle";
+    import DataTable from "../../components/dataTable/dataTable";
+    import ItemTable from "../../components/itemTable/itemTable";
     export default {
         name: "preBuildProject",
-      components: {TopHandle},
+      components: {ItemTable, DataTable, TopHandle},
       data(){
           return{
-            tableData:[{
-              id:'001',
-              name:'alice',
-              age:'20',
-              job:'web'
-            },{
-              id:'001',
-              name:'alice',
-              age:'20',
-              job:'web'
-            },{
-              id:'001',
-              name:'alice',
-              age:'20',
-              job:'web'
-            },{
-              id:'001',
-              name:'alice',
-              age:'20',
-              job:'web'
-            },{
-              id:'001',
-              name:'alice',
-              age:'20',
-              job:'web'
-            },{
-              id:'001',
-              name:'alice',
-              age:'20',
-              job:'web'
-            }],
-            highlightCurrentRow:true
+            table:{
+              tableData:[
+                {
+                  id:'001',
+                  name:'alice',
+                  age:'20',
+                  job:'web',
+                  end:"lhasflasdaafasfasfasfsfasfasfasfasfasfasfasfasfasvasasfgasgasgasfasgasfasfkj"
+                },{
+                  id:'001',
+                  name:'alice',
+                  age:'20',
+                  job:'web',
+                  end:"lhasflkj"
+                },{
+                  id:'001',
+                  name:'alice',
+                  age:'20',
+                  job:'web',
+                  end:"lhasflkj"
+                },{
+                  id:'001',
+                  name:'alice',
+                  age:'20',
+                  job:'web',
+                  end:"lhasflkj"
+                },{
+                  id:'001',
+                  name:'alice',
+                  age:'20',
+                  job:'web',
+                  end:"lhasflkj"
+                },{
+                  id:'001',
+                  name:'alice',
+                  age:'20',
+                  job:'web',
+                  end:"lhasflkj"
+                }],
+              colum:[
+                {
+                prop:'id',
+                label:'申报部门',
+                align:'left'
+              },{
+                prop:'name',
+                label:'预算部门',
+              },{
+                prop:'age',
+                label:'项目编码',
+                align:'center'
+              },{
+                prop:'job',
+                label:'项目名称',
+                align:'center'
+              },{
+                  prop:'job',
+                  label:'项目金额',
+                  align:'center'
+                },{
+                  prop:'job',
+                  label:'支持类别',
+                  align:'center'
+                },{
+                  prop:'job',
+                  label:'起止日期',
+                  align:'center'
+                },{
+                  prop:'job',
+                  label:'申报日期',
+                  align:'center'
+                },{
+                  prop:'job',
+                  label:'审批状态',
+                  align:'center'
+                }],
+              selection:true
+            },
+            table1:{
+              tableData:[]
+            }
           }
+      },
+      created(){
+        let a = {
+          item:{
+            projectCode:"1313",
+            projectName:"文书、人事档案整理装订",
+            projectMoney:'500',
+            name1:'新增项目',
+            name2:'经常性项目',
+            name3:"一级",
+            name4:'2019-01-01至2019-12-30',
+            name5:'机关行政类',
+            name6:'否',
+            name7:"组织部",
+            name8:'部门A',
+            name9:'2018-12-18'
+          },
+          status:'审批中'
+        }
+        for (let i =0;i<6;i++){
+          this.table1.tableData.push(a)
+        }
+      },
+      mounted(){
+
       },
       methods:{
         //表格行样式回调
@@ -88,9 +160,11 @@
         },
         //单元格样式回调
         cellClassName({row,column,rowIndex,columnIndex}){
+          let a =  Object.getOwnPropertyNames(row).length
+          console.log(column)
           if (columnIndex === 0){
             return 'frist-column'
-          }else if(columnIndex === Object.getOwnPropertyNames(row).length -2){
+          }else if(columnIndex === this.rowNumber-1){
             return 'last-column'
           }else {
             return 'middle-column'
@@ -102,6 +176,16 @@
             if (columnIndex === 0){
 
             }
+        },
+        fn(){
+          this.table.colum.push({
+            prop:'end',
+            label:'测试',
+            align:'left'
+          })
+        },
+        fn1(){
+
         }
       }
     }
@@ -114,59 +198,5 @@
   }
 </style>
 <style>
-  /*表格每一行样式*/
-  .row-class-name{
-    box-shadow: 0 0px 8px #cbcbcb;
-    border-radius: 8px;
-  }
-  .row-class-name:hover{
-    box-shadow: 0 0px 9px #409eff;
-  }
-  .el-table__row:hover>td{
-    border-top: 1px solid  #00b7ee !important;
-    border-bottom: 1px solid  #00b7ee !important;
-  }
-  .el-table__row:hover>.frist-column{
-    border-left: 1px solid #00b7ee !important;
-  }
-  .el-table__row:hover>.last-column{
-    border-right: 1px solid #00b7ee !important;
-  }
-  .row-class-name td{
-    border-bottom: none!important;
-    border-top: 1px solid #e3e3e3 !important;
-    border-bottom: 1px solid #e3e3e3 !important;
-    border-right: 1px solid #e3e3e3 !important;
-    padding: 5px 0;
-  }
-  .row-class-name td .cell{
-    height: 30px;
-    line-height: 30px;
-  }
-  /*调整表格每一行间距*/
-  .el-table__body{
-    border-collapse: separate;
-    border-spacing:0px 10px ;
-  }
-  /*第一列数据*/
-  .frist-column{
-    width: 150px !important;
-    border-left: 1px solid #e3e3e3 !important;
-    border-top-left-radius: 8px;
-    border-bottom-left-radius: 8px;
-  }
-  /*最后一列数据*/
-  .last-column{
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-  }
-  /*表头单元格*/
-  .thead-cell{
-    border-bottom: none !important;
-    background-color: #CACACA !important;
-    border-right: 1px solid #ffffff !important;
-  }
-  .current-row td{
-    background-color: #409eff !important;
-  }
+
 </style>
