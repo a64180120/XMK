bcf31467b24e4d9c8e75c24cc4e20d8c415e8dfb
xief <a64180120@163.com>
2019-07-18 20:04:43 +0800

    修改图片

--- src/pages/projectManage/index.vue
+++ src/pages/projectManage/index.vue
@@ -8,61 +8,73 @@
       <div class="pageNav">
         <div class="project">
           <div class="navTitle">
-            <div><img src="@/assets/images/xmgl.png"
-                   alt=""></div>
-            <div>项目管理</div>
+            <div>
+              <div><img src="@/assets/images/xmgl.png"
+                     alt=""></div>
+              <div>项目管理</div>
+            </div>
+
           </div>
-          <ul class="navlist">
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>预立项</div>
-            </li>
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>项目立项</div>
-            </li>
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>项目查询</div>
-            </li>
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>汇总统计</div>
-            </li>
-          </ul>
+          <div class="navlistCon">
+            <ul class="navlist">
+              <li @click="routerTo('/preBuildProject')">
+                <div><img src="@/assets/images/yulix.png"
+                       alt=""></div>
+                <div>预立项</div>
+              </li>
+              <li>
+                <div><img src="@/assets/images/xmlix.png"
+                       alt=""></div>
+                <div>项目立项</div>
+              </li>
+              <li @click="routerTo('/projectList')">
+                <div><img src="@/assets/images/xmcx.png"
+                       alt=""></div>
+                <div>项目查询</div>
+              </li>
+              <li>
+                <div><img src="@/assets/images/hztj.png"
+                       alt=""></div>
+                <div>汇总统计</div>
+              </li>
+            </ul>
+          </div>
+
         </div>
         <div class="report">
           <div class="navTitle">
-            <div><img src="@/assets/images/ysgl.png"
-                   alt=""></div>
-            <div>预算管理</div>
+            <div>
+              <div><img src="@/assets/images/ysgl.png"
+                     alt=""></div>
+              <div>预算管理</div>
+            </div>
+
           </div>
-          <ul class="navlist">
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>年初申报</div>
-            </li>
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>年中调整</div>
-            </li>
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>预算报表</div>
-            </li>
-            <li>
-              <div><img src=""
-                     alt=""></div>
-              <div>进度控制</div>
-            </li>
-          </ul>
+          <div class="navlistCon">
+            <ul class="navlist">
+              <li @click="routerTo('/yearreport')">
+                <div><img src="@/assets/images/ncsb.png"
+                       alt=""></div>
+                <div>年初申报</div>
+              </li>
+              <li>
+                <div><img src="@/assets/images/nztz.png"
+                       alt=""></div>
+                <div>年中调整</div>
+              </li>
+              <li>
+                <div><img src="@/assets/images/ysbb.png"
+                       alt=""></div>
+                <div>预算报表</div>
+              </li>
+              <li @click="routerTo('/progresscontrol')">
+                <div><img src="@/assets/images/jdkz.png"
+                       alt=""></div>
+                <div>进度控制</div>
+              </li>
+            </ul>
+          </div>
+
         </div>
       </div>
     </div>
@@ -76,6 +88,9 @@ export default {
   methods: {
     refresh () {
 
+    },
+    routerTo (str) {
+      this.$router.push({ path: str })
     }
   },
   components: {
@@ -115,36 +130,78 @@ export default {
   .navTitle {
     float: left;
     width: 20%;
-    min-width: 250px;
+    padding: 10px;
+    // min-width: 250px;
     height: 100%;
-    background: $btnColor;
     color: #fff;
     text-align: center;
     > div {
-      height: 170px;
-      line-height: 170px;
-      > img {
-        vertical-align: middle;
-        width: 100%;
-        max-width: 120px;
-        max-height: 120px;
-        height: 100%;
+      width: 100%;
+      height: 100%;
+      box-shadow: 0 0 10px #888;
+      border-radius: 3px;
+      overflow: hidden;
+      > div {
+        height: 160px;
+        line-height: 160px;
+        background: $btnColor;
+        > img {
+          vertical-align: middle;
+          width: 100%;
+          max-width: 120px;
+          max-height: 120px;
+          height: 100%;
+        }
+      }
+      > div:nth-of-type(2) {
+        background: $btnColor;
+        height: 50px;
+        line-height: 50px;
       }
     }
-    > div:nth-of-type(2) {
-      height: 50px;
-      line-height: 50px;
-    }
+  }
+  .navlistCon {
+    float: left;
+    width: 80%;
+    height: 100%;
+
+    display: inline-block;
+    overflow-x: auto;
+    overflow-y: hidden;
   }
   .navlist {
-    float: left;
-    // width: 80%;
     height: 100%;
-    display: inline-block;
+    padding: 10px;
     white-space: nowrap;
+    color: $btnColor;
     > li {
-      float: left;
+      display: inline-block;
       width: 220px;
+      height: 100%;
+      text-align: center;
+      border-radius: 3px;
+      overflow: hidden;
+      cursor: pointer;
+      margin-right: 10px;
+      box-shadow: 0 0 5px #ccc;
+      &:hover {
+        box-shadow: 0 0 5px $btnColor;
+      }
+      > div {
+        height: 160px;
+        line-height: 160px;
+        > img {
+          vertical-align: middle;
+          width: 100%;
+          max-width: 120px;
+          max-height: 120px;
+          height: 100%;
+        }
+      }
+      > div:nth-of-type(2) {
+        height: 50px;
+        line-height: 50px;
+      }
     }
   }
 }
