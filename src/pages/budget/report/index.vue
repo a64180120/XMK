f103f7ceac53c31826727a2c39f939c116298cf7
hongyz <123@123>
2019-07-19 12:00:01 +0800

    页面添加最小宽度

--- src/pages/budget/report/index.vue
+++ src/pages/budget/report/index.vue
@@ -1,56 +1,12 @@
 <template>
-  <section>
+  <section class="report">
     <div style="position: relative">
-      <tophandle title="预算中心"
-                 @refresh="getData">
+      <tophandle title="预算中心" @refresh="getData">
         <div class="btnCon">
-<!--          <div @click.stop="showAuditAdd('add')"
-               class="handle">
-            <div class="topIcon"><img src="@/assets/images/xz.png"
-                                      alt=""></div>
-            新增
-          </div>
-          <div @click.stop="showAuditAdd('update')"
-               class="handle">
-            <div class="topIcon"><img src="@/assets/images/zj2.png"
-                                      alt=""></div>
-            修改
-          </div>
-          <div @click.stop="showAuditAdd('delete')"
-               class="handle">
-            <div class="topIcon"><img src="@/assets/images/zj3.png"
-                                      alt=""></div>
-            删除
-          </div>
-          <div @click.stop="showAuditAdd('SS')"
-               class="handle">
-            <div class="topIcon"><img src="@/assets/images/sp.png"
-                                      alt=""></div>&lt;!&ndash; @click.stop="approvalDataS.openDialog=true"&ndash;&gt;
-            送审
-          </div>
-          <div @click.stop="showAuditAdd('QXSS')"
-               class="handle">
-            <div class="topIcon"><img src="@/assets/images/ss_d.png"
-                                      alt=""></div>&lt;!&ndash; @click.stop="approvalDataS.openDialog=true"&ndash;&gt;
-            取消送审
-          </div>
-          <div @click.stop="showAuditAdd('SC')"
-               class="handle"
-               style="width: 80px;">
-            <div class="topIcon"><img src="@/assets/images/sc.png"
-                                      alt=""></div>&lt;!&ndash; @click="creatPayItem()"&ndash;&gt;
-            生成支付单
-          </div>
-          <div @click.stop="showAuditAdd('ZF')"
-               class="handle">
-            <div class="topIcon"><img src="@/assets/images/zf.png"
-                                      alt=""></div>&lt;!&ndash; @click.stop="approvalDataS.openDialog=true"&ndash;&gt;
-            作废
-          </div>-->
           <div @click.stop="printTables" class="handle" style="width: 80px;">
             <div class="topIcon">
               <img src="@/assets/images/dy.png" alt="">
-            </div><!-- @click="creatPayItem()"-->
+            </div>
             打印
           </div>
         </div>
@@ -61,7 +17,9 @@
       <div class="leftNav">
         <ul>
           <template v-for="(item,index) in navList">
-            <li @click="navActive=index" :class="{active:navActive==index}">{{item.title}}</li>
+            <li :style="{height:(100/navList.length)+'%'}" @click="navActive=index" :class="{active:navActive==index}">
+              <p>{{item.title}}</p>
+            </li>
           </template>
         </ul>
       </div>
@@ -71,6 +29,7 @@
 </template>
 
 <script>
+  import tophandle from '@/components/topNav/topHandle'
   export default {
     name: "yearreport",
     data(){
@@ -85,6 +44,63 @@
         //选中的导航
         navActive:0
       }
-    }
+    },
+
+    components:{tophandle},
   }
 </script>
+<style lang="scss" >
+  .btnCon{
+    .handle{
+      display: inline-block;
+      width: 60px;
+      height: 50px;
+      padding: 0px 0;
+      cursor: pointer;
+      &:active{
+        background: #ccc;
+      }
+    }
+  }
+  .report{
+    >.container{
+      >.leftNav{
+        height: 100%;
+        width: .4rem;
+        float: left;
+        display: inline-block;
+        &:after{
+          content: '';
+          clear: both;
+        }
+        >ul{
+          height: 100%;
+          >li{
+            background-color: #cccccc;
+            box-sizing: border-box;
+            border-bottom: 1px solid #fff;
+            text-align: center;
+            box-shadow: inset 0px 0px 10px 0px #bbb;
+            &.active{
+              background-color: #00B8EE;
+              color: #fff;
+              box-shadow: inset 0px 0px 10px 0px #f6fdff;
+            }
+            &:before{
+              content: '';
+              height: 130%;
+              vertical-align: middle;
+              display: inline-block;
+            }
+            >p{
+              display: inline-block;
+              line-height: 20px;
+            }
+          }
+
+        }
+      }
+    }
+  }
+
+</style>
